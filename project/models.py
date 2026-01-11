6058a700c10464a7d071121beb4020c011a1bf55
nalouddin <nalouddin@gmail.com>
2026-01-11 22:29:18 +0500

    first commit

---
+++ project/models.py
@@ -0,0 +1,44 @@
+import uuid
+
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=400, null=True, blank=True)
+    source_link = models.CharField(max_length=400, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    vote_count = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    tags = models.ManyToManyField('Tag', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('+', 'Ijobiy'),
+        ('-', 'Salbiy'),
+    )
+
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=10, choices=VOTE_TYPE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return f"{self.value} - {self.product.title}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
